57bc919140c53a9916d8f054a8fe0204b20672b2
AaronTheNerd <bargeaaron@gmail.com>
2024-01-30 06:38:39 -0500

    Fix: Improved point equality testing

--- src/point/ABCs.py
+++ src/point/ABCs.py
@@ -13,3 +13,6 @@ class PointABC(ABC):
     @abstractmethod
     def at(self, t: float) -> PointABC:
         ...
+
+    def __eq__(self, value: PointABC) -> bool:
+        return value is self or (value.x == self.x and value.y == self.y)
