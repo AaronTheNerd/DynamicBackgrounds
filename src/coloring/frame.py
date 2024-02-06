34af3ed542d480b75121250211f30ecbdd869958
AaronTheNerd <bargeaaron@gmail.com>
2024-02-06 18:20:40 -0500

    Chore: Continued cleaning the image drawing portion of the program

---
+++ src/coloring/frame.py
@@ -0,0 +1,114 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import Optional
+
+from PIL import Image, ImageDraw
+
+from coloring.edge import EdgeDrawer
+from coloring.triangle import TriangleDrawer
+from coloring.vertex import VertexDrawer
+from configs import CONFIGS
+from log.performance import measure
+from point.ABCs import PointABC
+from serial.ABCs import SerialABC
+from serial.JSON_types import JSON_object
+from triangle import Triangle
+
+
+@dataclass
+class FrameDrawer(SerialABC):
+    triangle_drawer: Optional[TriangleDrawer]
+    edge_drawer: Optional[EdgeDrawer]
+    vertex_drawer: Optional[VertexDrawer]
+
+    @classmethod
+    def from_json(
+        cls,
+        triangle_coloring: JSON_object,
+        line_coloring: JSON_object,
+        point_coloring: JSON_object,
+    ) -> FrameDrawer:
+        triangle_drawer = None
+        if triangle_coloring is not None:
+            triangle_drawer = TriangleDrawer.from_json(**triangle_coloring)
+        edge_drawer = None
+        if line_coloring is not None:
+            edge_drawer = EdgeDrawer.from_json(**line_coloring)
+        vertex_drawer = None
+        if point_coloring is not None:
+            vertex_drawer = VertexDrawer.from_json(**point_coloring)
+        return cls(triangle_drawer, edge_drawer, vertex_drawer)
+
+    def draw(
+        self,
+        points: list[PointABC],
+        triangles: list[Triangle],
+        time: float,
+    ) -> Image.Image:
+        frame = Image.new(
+            "RGB",
+            (CONFIGS.full_width, CONFIGS.full_height),
+            tuple(CONFIGS.gif.background_color),
+        )
+        frame_draw = ImageDraw.Draw(frame)
+        if self.triangle_drawer is not None:
+            self.draw_triangles(frame_draw, triangles, time)
+        if self.edge_drawer is not None:
+            self.draw_lines(frame_draw, triangles, time)
+        if self.vertex_drawer is not None:
+            self.draw_points(frame_draw, points, time)
+        return frame
+
+    @measure
+    def draw_triangles(
+        self,
+        frame_draw: ImageDraw.ImageDraw,
+        triangles: list[Triangle],
+        time: float,
+    ) -> None:
+        for triangle in triangles:
+            triangle_color = self.triangle_drawer.get_color(triangle, time)
+            frame_draw.polygon(
+                [
+                    triangle.a.x,
+                    triangle.a.y,
+                    triangle.b.x,
+                    triangle.b.y,
+                    triangle.c.x,
+                    triangle.c.y,
+                ],
+                fill=tuple(triangle_color),
+            )
+
+    @measure
+    def draw_lines(
+        self, frame_draw: ImageDraw.ImageDraw, triangles: list[Triangle], time: float
+    ) -> None:
+        for triangle in triangles:
+            edges = triangle.edges()
+            for edge in edges:
+                color = self.edge_drawer.get_color(edge, time)
+                width = self.edge_drawer.get_width(edge, time)
+                frame_draw.line(
+                    [edge.a.x, edge.a.y, edge.b.x, edge.b.y],
+                    width=width,
+                    fill=tuple(color),
+                )
+
+    @measure
+    def draw_points(
+        self, frame_draw: ImageDraw.ImageDraw, points: list[PointABC], time: float
+    ) -> None:
+        for point in points:
+            color = self.vertex_drawer.get_color(point, time)
+            radius = int(self.vertex_drawer.get_width(point, time) / 2)
+            frame_draw.ellipse(
+                [
+                    point.x - radius,
+                    point.y - radius,
+                    point.x + radius,
+                    point.y + radius,
+                ],
+                fill=tuple(color),
+            )
--- src/main.py
+++ src/main.py
@@ -4,20 +4,16 @@ import os
 
 import numpy as np
 from opensimplex import OpenSimplex
-from PIL import Image, ImageDraw
 
 import point.generator.generate as generate_points
-from coloring.edge import EdgeDrawer
-from coloring.triangle import TriangleDrawer
-from coloring.vertex import VertexDrawer
+from coloring.frame import FrameDrawer
 from configs import CONFIGS
-from draw import draw
 from log.enable import enable_logging
 from triangulation.triangulation import get_triangulation
 from utils.progress_bar import progress_bar
 
 
-def run():
+def main():
     enable_logging()
     logger = logging.getLogger(__name__)
     output_dir = os.path.join(
@@ -31,43 +27,26 @@ def run():
     generate_points.seed(CONFIGS.seed)
     open_simplex = OpenSimplex(CONFIGS.seed)
     # Create copy of configs to be able to remake the gif
-    with open(f"{output_dir}/config.json", "w+") as file:
+    with open(os.path.join(output_dir, "config.json"), "w+") as file:
         json.dump(CONFIGS.dumpJSON(), file, indent=2)
     triangulation = get_triangulation(CONFIGS.triangulation)
-    # Generate objects needed to color the gif
-    triangle_coloring = None
-    if CONFIGS.triangle_coloring:
-        triangle_coloring = TriangleDrawer.from_json(**CONFIGS.triangle_coloring)
-    line_coloring = None
-    if CONFIGS.line_coloring:
-        line_coloring = EdgeDrawer.from_json(**CONFIGS.line_coloring)
-    point_coloring = None
-    if CONFIGS.point_coloring:
-        point_coloring = VertexDrawer.from_json(**CONFIGS.point_coloring)
+    frame_drawer = FrameDrawer.from_json(
+        CONFIGS.triangle_coloring, CONFIGS.line_coloring, CONFIGS.point_coloring
+    )
     # Generate initial points
     points = generate_points.generate_points(open_simplex)
     # Generate frames
     for i, t in enumerate(
         np.linspace(0.0, 1.0, CONFIGS.gif.num_of_frames, endpoint=False)
     ):
-        image = Image.new(
-            "RGB",
-            (CONFIGS.full_width, CONFIGS.full_height),
-            tuple(CONFIGS.gif.background_color),
-        )
-        image_draw = ImageDraw.Draw(image)
         progress_bar(t)
-        draw(
-            image_draw,
-            t,
-            points,
-            triangulation,
-            triangle_coloring,
-            line_coloring,
-            point_coloring,
+        new_points = [point.at(t) for point in points]
+        triangles = triangulation(new_points)
+        frame = frame_drawer.draw(new_points, triangles, t)
+        file_name = os.path.join(
+            output_dir, f"image#{str(i).zfill(3)}.{CONFIGS.gif.file_extension}"
         )
-        file_name = f"{output_dir}/image#{str(i).zfill(3)}.{CONFIGS.gif.file_extension}"
-        image.save(file_name)
+        frame.save(file_name)
     # Convert frames to gif
     progress_bar(1.0)
     print()
@@ -80,4 +59,4 @@ def run():
 
 
 if __name__ == "__main__":
-    run()
+    main()
