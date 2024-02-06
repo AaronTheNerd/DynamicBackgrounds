8452134dcc2f20f50c877f58068dd47a6e3c9d28
AaronTheNerd <bargeaaron@gmail.com>
2024-02-05 19:00:12 -0500

    Chore: cleaned the draw() function

--- src/main.py
+++ src/main.py
@@ -13,6 +13,7 @@ from coloring.triangle import TriangleDrawer
 from coloring.vertex import VertexDrawer
 from configs import CONFIGS
 from point.ABCs import PointABC
+from triangle import Edge, Triangle
 from utils.progress_bar import progress_bar
 
 SRC_PATH = os.path.abspath(os.path.dirname(__file__))
@@ -20,7 +21,7 @@ GIFS_PATH = f"{SRC_PATH}/../gifs"
 
 
 def draw(
-    image,
+    image: ImageDraw.ImageDraw,
     t: float,
     points: list[PointABC],
     triangle_coloring: Optional[TriangleDrawer],
@@ -30,43 +31,69 @@ def draw(
     new_points = [point.at(t) for point in points]
     triangles = BowyerWatson(new_points)
     if triangle_coloring is not None:
-        for triangle in triangles:
-            triangle_color = triangle_coloring.get_color(triangle, t)
-            image.polygon(
-                [
-                    triangle.a.x,
-                    triangle.a.y,
-                    triangle.b.x,
-                    triangle.b.y,
-                    triangle.c.x,
-                    triangle.c.y,
-                ],
-                fill=tuple(triangle_color),
-            )
+        draw_triangles(image, triangle_coloring, triangles, t)
+
     if line_coloring is not None:
         for triangle in triangles:
             edges = triangle.edges()
-            for edge in edges:
-                color = line_coloring.get_color(edge, t)
-                width = line_coloring.get_width(edge, t)
-                image.line(
-                    [edge.a.x, edge.a.y, edge.b.x, edge.b.y],
-                    width=width,
-                    fill=tuple(color),
-                )
+            draw_lines(image, line_coloring, edges, t)
+
     if point_coloring is not None:
-        for point in new_points:
-            color = point_coloring.get_color(point, t)
-            radius = int(point_coloring.get_width(point, t) / 2)
-            image.ellipse(
-                [
-                    point.x - radius,
-                    point.y - radius,
-                    point.x + radius,
-                    point.y + radius,
-                ],
-                fill=tuple(color),
-            )
+        draw_points(image, point_coloring, new_points, t)
+
+
+def draw_triangles(
+    image: ImageDraw.ImageDraw,
+    triangle_coloring: TriangleDrawer,
+    triangles: list[Triangle],
+    t: float,
+) -> None:
+    for triangle in triangles:
+        triangle_color = triangle_coloring.get_color(triangle, t)
+        image.polygon(
+            [
+                triangle.a.x,
+                triangle.a.y,
+                triangle.b.x,
+                triangle.b.y,
+                triangle.c.x,
+                triangle.c.y,
+            ],
+            fill=tuple(triangle_color),
+        )
+
+
+def draw_lines(
+    image: ImageDraw.ImageDraw, line_coloring: EdgeDrawer, edges: list[Edge], t: float
+) -> None:
+    for edge in edges:
+        color = line_coloring.get_color(edge, t)
+        width = line_coloring.get_width(edge, t)
+        image.line(
+            [edge.a.x, edge.a.y, edge.b.x, edge.b.y],
+            width=width,
+            fill=tuple(color),
+        )
+
+
+def draw_points(
+    image: ImageDraw.ImageDraw,
+    point_coloring: VertexDrawer,
+    points: list[PointABC],
+    t: float,
+) -> None:
+    for point in points:
+        color = point_coloring.get_color(point, t)
+        radius = int(point_coloring.get_width(point, t) / 2)
+        image.ellipse(
+            [
+                point.x - radius,
+                point.y - radius,
+                point.x + radius,
+                point.y + radius,
+            ],
+            fill=tuple(color),
+        )
 
 
 def run():
