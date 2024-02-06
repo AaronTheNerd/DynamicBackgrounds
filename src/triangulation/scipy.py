f624cb79595563722b32605aeda9d95216446c5d
AaronTheNerd <bargeaaron@gmail.com>
2024-02-06 05:55:31 -0500

    Chore: Refactored scipy triangulation code

--- src/triangulation/scipy.py
+++ src/triangulation/scipy.py
@@ -9,15 +9,18 @@ from scipy.spatial import Delaunay
 def SciPy(points: list[PointABC]) -> list[Triangle]:
     scipy_points = convert_to_scipy_inputs(points)
     scipy_triangulation = Delaunay(scipy_points)
-    indicies = scipy_triangulation.simplices
-    triangles = []
-    for triangle_indicies in indicies:
-        triangle_points = [points[index] for index in triangle_indicies]
-        triangles.append(Triangle(*triangle_points))
-    return triangles
+    return convert_scipy_triangulation(scipy_triangulation, points)
 
 def convert_to_scipy_inputs(points: list[PointABC]) -> numpy.ndarray:
     return numpy.array([scipy_point(point) for point in points])
 
 def scipy_point(point: PointABC) -> list[float]:
     return [point.x, point.y]
+
+def convert_scipy_triangulation(delaunay: Delaunay, points: list[PointABC]) -> list[Triangle]:
+    indicies = delaunay.simplices
+    triangles = []
+    for triangle_indicies in indicies:
+        triangle_points = [points[index] for index in triangle_indicies]
+        triangles.append(Triangle(*triangle_points))
+    return triangles
